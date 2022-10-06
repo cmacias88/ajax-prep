39be5f15aa079810257702f81a88b5ee2effdc4a
Chloe Macias <chloeamacias@gmail.com>
2022-10-06 16:32:33 -0400

    Completed profile.js.

--- static/profile.js
+++ static/profile.js
@@ -1,14 +1,26 @@
+
 function submitProfile(evt) {
   evt.preventDefault();
 
   const data = {
     name: document.querySelector('#name-field').value,
-    // fill in the rest
+    age: document.querySelector('#age-field').value,
+    occupation : document.querySelector('#occupation-field').value,
   };
 
-  // make request to server to get the data
-  // add the data the server returns to the document
-  // (you can add it to the end of the div with ID "profiles")
+  fetch('/api/profile', {
+    method: 'POST',
+    body: JSON.stringify(data), 
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then((response) => response.json())
+    .then((formJSON) => {
+      document.querySelector('#profiles').insertAdjacentHTML('beforeend', `<p>${formJSON.name} is ${formJSON.age}, 
+      working as a ${formJSON.occupation}.</p>`);
+    });
+
 }
 
 document.querySelector('#profile-form').addEventListener('submit', submitProfile);
